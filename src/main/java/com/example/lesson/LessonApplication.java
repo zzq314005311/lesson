14f6e3e89a532ae673c0e508682fcaba14749ba5
zzq <314005311@qq.com>
2018-11-29 08:17:30 +0800

    Initial commit

---
+++ src/main/java/com/example/lesson/LessonApplication.java
@@ -0,0 +1,12 @@
+package com.example.lesson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LessonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LessonApplication.class, args);
+    }
+}
